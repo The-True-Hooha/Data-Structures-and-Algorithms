9e7dd93c745dbd4b03b6e06e11034e56b1a5d973
David Ogar <owogogahhero@outlook.com>
2022-06-28 11:40:48 +0100

    refactor: moved question to single class

--- src/com/github/TheTrueHooha/Main.java
+++ src/com/github/TheTrueHooha/Main.java
@@ -1,15 +1,36 @@
 package com.github.TheTrueHooha;
 
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        FindTheSecondLargestNumber findTheSecondLargestNumber = new FindTheSecondLargestNumber();
-
-
+        findSecondLargestNumber findTheSecondLargestNumber = new findSecondLargestNumber();
         //calls an object of the class
-        findTheSecondLargestNumber.number();
+        findTheSecondLargestNumber.solution();
     }
 
+}
+
+class findSecondLargestNumber{
+
+    public void solution() {
+        int [] numbers = {1, 22, 3, 1, 22, 5, 5, 6, 6, 77, 77, 4, 4, 8};
+
+        //sorts the number
+        SortedSet<Integer> sortNumber = new TreeSet<>();
+        //loop through the array and sort the number
+
+        for (Integer i : numbers) {
+            sortNumber.add(i);
+            System.out.println(sortNumber);
+        }
+
+        //prints the second-largest number
+        sortNumber.remove(sortNumber.last());
+        System.out.println(sortNumber.last());
+    }
 }
