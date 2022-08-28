c8569103918ef8c192a2f14bced163a23cf9e446
David Ogar <owogogahhero@outlook.com>
2022-08-28 19:25:36 +0100

    feat: implemented search and sort

--- src/Foundation/Array/ArraySort.java
+++ src/Foundation/Array/ArraySort.java
@@ -15,5 +15,27 @@ public class ArraySort {
         for (int i = 0; i<arraySort.length; i++){ // loops through the new sorted array and prints them
             System.out.print(arraySort[i]);
         }
+
+        // calls the searchArray method and takes in a value and a key
+
+        int value =  searchArray(arraySort, 2);
+        if (value == -1){
+            System.out.println("value cannot be found in the array");
+        } else {
+            // TODO: print result in a new line out the sorted results
+            System.out.println("value found here at index "+value);
+        }
+    }
+
+    // search through an array
+    // pass as static inside a main class
+    // TODO: can you have two public methods inside a main class?
+    public static int searchArray(int arraySearch[], int key){
+        for (int i=0; i<arraySearch.length; i++){
+            if (arraySearch[i] == key){
+                return i;
+            }
+        }
+        return -1;
     }
 }
