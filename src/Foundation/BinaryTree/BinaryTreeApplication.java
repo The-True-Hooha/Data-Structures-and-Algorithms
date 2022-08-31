a62560e5ea4b0f546b2b777ee11f60431dfe8c09
David Ogar <owogogahhero@outlook.com>
2022-08-31 07:41:30 +0100

    feat: binary tree implementation

--- src/Foundation/BinaryTree/BinaryTreeApplication.java
+++ src/Foundation/BinaryTree/BinaryTreeApplication.java
@@ -1,2 +1,41 @@
-package Foundation.BinaryTree;public class BinaryTreeApplication {
+package Foundation.BinaryTree;
+
+public class BinaryTreeApplication {
+    public static void main(String[] args) {
+        TreeTraversal tree = new TreeTraversal();
+
+        // sets the binary tree from A, B, C, D, E
+        tree.parentNode = new ParentNode('A');
+        tree.parentNode.left = new ParentNode('B');
+        tree.parentNode.right = new ParentNode('C');
+        tree.parentNode.left.left = new ParentNode('D');
+        tree.parentNode.left.right = new ParentNode('E');
+
+        // calls the preOrderTraversal pattern
+        tree.preOrderTraversal(tree.parentNode);
+    }
+}
+
+// class parent for parent node
+class ParentNode {
+    char key;
+    ParentNode left, right;
+
+    ParentNode(char key){
+        this.key = key;
+    }
+}
+
+// TreTraversal describes the Binary Tree operation
+class TreeTraversal{
+    ParentNode parentNode;
+
+    // for preOrderTraversal: the order goes for left, root node, and right
+    void preOrderTraversal(ParentNode node){
+        if (node != null ){ // checks the parent node
+            System.out.println(node.key+ "");
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+        }
+    }
 }
