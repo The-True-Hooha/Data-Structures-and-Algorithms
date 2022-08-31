8b50160de5bfab3a67909835aa22bca90225a4b7
David Ogar <owogogahhero@outlook.com>
2022-08-31 07:43:35 +0100

    fix: queue implementation

--- src/Foundation/Queue/QueueApplication.java
+++ src/Foundation/Queue/QueueApplication.java
@@ -3,8 +3,18 @@ package Foundation.Queue;
 public class QueueApplication {
     public static void main(String[] args) {
 
+        QueueWithArray queueWithArray = new QueueWithArray(5);
+        queueWithArray.addQueue(6);
+        queueWithArray.addQueue(3);
+        queueWithArray.addQueue(2);
+        queueWithArray.addQueue(7);
+        queueWithArray.showQueue();
+        queueWithArray.removeQueue();
     }
 }
+
+
+
 class QueueWithArray{
     int start, end;
     int range;
@@ -25,12 +35,25 @@ class QueueWithArray{
         }
     }
 
+    // dequeue method
     void removeQueue(){
         if (end == 0){
             System.out.println("the queue is empty");
         } else {
-            for (int i = 0; i < end - 1; end++){
+            for (int i = 0; i < end - 1; i++){
+                example[i] = example[i+1];
+            }
+            end--;
+        }
+    }
 
+    // shows the current element on the queue
+    void showQueue(){
+        if (end == 0){
+            System.out.println("oops, the queue is empty or has no elemnts");
+        } else {
+            for (int i = 0; i<end; i++){
+                System.out.print(example[i]+ "");
             }
         }
     }
