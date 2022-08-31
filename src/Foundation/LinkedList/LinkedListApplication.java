6d1e16e413be8c5560b4e04f7de6b3d40bbc3d0d
David Ogar <owogogahhero@outlook.com>
2022-08-31 07:43:01 +0100

    feat: add linked list implementation

--- src/Foundation/LinkedList/LinkedListApplication.java
+++ src/Foundation/LinkedList/LinkedListApplication.java
@@ -1,2 +1,32 @@
-package Foundation.LinkedList;public class LinkedListApplication {
+package Foundation.LinkedList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import static java.lang.System.*;
+
+public class LinkedListApplication {
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+        // primitive operations to add an element
+        list.add("come");
+        list.add("hi");
+        list.add("by");
+        list.add(3, "list"); // add the element at the index position specified
+
+        list.remove(1); // removes the first element || the position if specified
+        // list.removeFirst(); removes the element at the top
+        // list.removeLast(); removes the last element
+
+        Iterator<String> loop = list.iterator();
+        while (loop.hasNext()) {
+             out.println(loop.next()); // prints the next element
+        }
+        out.println(list.size()); // prints the size of the linked list
+
+        // search through a linked list the returns a bool data type if present or not
+        boolean bool = list.contains("go");
+        out.println(bool);
+    }
 }
